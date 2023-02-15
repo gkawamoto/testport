e6d5e77dc1554e3a00c8c758bb4643c8890a6132
Gustavo Kawamoto <gustavo.kawamoto@gmail.com>
2023-02-15 12:20:57 +0100

    Small but needed improvements over very old copy-pasta code

---
+++ README.md
@@ -0,0 +1,31 @@
+# testport
+
+Small utility to test ports, no matter what OS you're using.
+
+# Installation
+
+```
+go install github.com/gkawamoto/testport@latest
+```
+
+# Usage
+
+```bash
+$ testport -help
+USAGE: testport host port [-revert]
+```
+
+# Examples
+
+```bash
+$ testport localhost 22 && echo "ssh server running on localhost"
+```
+
+```bash
+$ testport -revert 192.168.1.2 3389 && echo "remote desktop not available at 
+```
+
+```bash
+$ # slow network, lets leave some breathing room
+$ testport -timeout 2000 201.48.33.80 3389 && echo "remote desktop not available at 
+```
--- main.go
+++ main.go
@@ -1,41 +1,56 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
-	"time"
 	"os"
+	"time"
 )
 
 func main() {
-	if len(os.Args) < 3 || len(os.Args) > 4 {
-		usage()
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "USAGE: %s host port [-revert] [-timeout milliseconds; default=500] [-help]\n", flag.CommandLine.Name())
+		os.Exit(3)
+	}
+
+	revert := flag.Bool("revert", false, "revert the exit code")
+	timeout := flag.Float64("timeout", 500, "timeout in milliseconds")
+
+	flag.Parse()
+
+	if flag.NArg() != 2 {
+		flag.Usage()
 		return
 	}
-	if len(os.Args) == 4 {
-		if os.Args[3] != "-revert" {
-			usage()
-			return
-		}
-	}
-	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%s", os.Args[1], os.Args[2]), 500 * time.Millisecond)
+
+	host := flag.Arg(0)
+	port := flag.Arg(1)
+
+	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%s", host, port), time.Duration(*timeout)*time.Millisecond)
 
 	if err, ok := err.(*net.OpError); ok && err.Timeout() {
+		if *revert {
+			os.Exit(0)
+			return
+		}
+
 		os.Exit(1)
 		return
 	}
 
 	if err != nil {
-		// Log or report the error here
-		fmt.Printf("error: %s\n", err)
+		fmt.Fprintf(os.Stderr, "error: %s\n", err)
 		os.Exit(2)
 		return
 	}
-	conn.Close()
-	os.Exit(0)
-}
 
-func usage() {
-	fmt.Fprintf(os.Stderr, "USAGE: %s host port [-revert]\n", os.Args[0])
-	os.Exit(1)
+	conn.Close()
+
+	if *revert {
+		os.Exit(1)
+		return
+	}
+
+	os.Exit(0)
 }
